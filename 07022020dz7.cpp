2e9c2c8c13f139c6c92ec3df0ab5c954cf257bf2
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 19:40:09 +0300

    Добавьте файлы проекта.

---
+++ 07022020dz7.cpp
@@ -0,0 +1,29 @@
+﻿/*
+Задание 7: Пользователь вводит два целых числа.Необходимо вывести все целые числа, на которые оба введенных числа делятся без остатка.
+*/
+#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	int num, num2, delitel = 2;
+	cout << "Введите первое целое число: \n";
+	cin >> num;
+	cout << "Введите второе целое число: \n";
+	cin >> num2;
+	cout << "Все целые числа, на которые оба введенных числа делятся без остатка:\n";
+	while (delitel <= num || delitel <= num2)
+	{
+		if (num % delitel == 0 && num2 % delitel == 0)
+		{
+			cout << delitel << "; ";
+		}
+		delitel++;
+	}
+	cout << endl;
+	return 0;
+}
